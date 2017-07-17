797638809ca9a09f4c32b7aa634cebd471af90c2
buiquangthang <buiquangthangit@gmail.com>
2017-07-17 11:41:11 +0700

    Refactors test headers for each request

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 describe Api::V1::UsersController do
-  before(:each) { request.headers['Accept'] = "application/vnd.marketplace.v1" }
 
   describe "GET #show" do
     before(:each) do
